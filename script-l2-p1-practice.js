eee786945b2bdc1992aed553752d5de68c833ea7
Sergij Suprunenko <soteach1982@gmail.com>
2022-08-01 20:45:02 +0300

    add p1l2

---
+++ script-l2-p1-practice.js
@@ -0,0 +1,17 @@
+//порахувати загальну суму покупок в кошику
+
+// const cart = [54, 28, 105, 70, 92, 17, 120];
+// // 2.
+// let total = 0;
+// // 1.перебрати масив
+// for (let i = 0; i < cart.length; i += 1) {
+//     console.log(cart[i]);
+//     total += cart[i];
+//     // total = total+cart[i];
+// }
+// // 2.Зробити змінну total до циклу
+
+// console.log("total :",total);
+// 3. Кожен елемент додати до total
+
+dddddddddd
---
+++ script-l2-p1.js
@@ -0,0 +1,238 @@
+// --------------------------------ітерація по масиву-----------------------------------
+
+// Масив - структура даних для зберігання і маніпулювання колекцією індексованих значень.
+// Використовується для зберігання впорядкованих колекцій даних, наприклад, списку курортів, товарів, клієнтів в готелі тощо.
+
+// цикл for
+
+// const clients = ["Mango", "Ajax", "Poly", "Ilon"];
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+
+// Цикл for...of
+
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (const client of clients) {
+//   console.log(client);
+// }
+
+// const string = "javascript";
+
+// for (const character of string) {
+//   console.log(character);
+// }
+
+// break і continue
+
+// const clients = ["Mango", "Poly", "Ajax"];
+// const clientNameToFind = "Ajax";
+// let message;
+
+// for (const client of clients) {
+//   // На кожній ітерації будемо перевіряти чи збігається елемент масиву з
+//   // іменем клієнта. Якщо збігається - записуємо в message повідомлення
+//   // про успіх і робимо break, щоб далі не шукати
+//   if (client === clientNameToFind) {
+//     message = "Клієнт з таким ім'ям є в базі даних!";
+//     break;
+//   }
+
+//   // Якщо вони не збігаються - записуємо в message повідомлення про відсутність імені
+//   message = "Клієнт з таким ім'ям відсутній в базі даних!";
+// }
+
+// console.log(message); // "Клієнт з таким ім'ям є в базі даних!"
+
+
+// ------------------------------виведення чисел більших за певне значення
+
+// const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+// const threshold = 15;
+
+// // Для чисел, менших ніж порогове значення, спрацьовує continue, виконання тіла
+// // припиняється і управління передається на наступну ітерацію.
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] < threshold) {
+//     continue;
+//   }
+
+//   console.log(`Число більше за ${threshold}: ${numbers[i]}`); // 18, 29, 34
+// }
+
+
+// -------------------------------Присвоювання за значенням і за посиланням----------------------------------------
+
+// ---------------------Примітиви (рядки, числа, булі, null і undefined) присвоюються за значенням----------------------------
+// let a = 5;
+// // Присвоєння за значенням, в пам'яті буде створена ще
+// // одна комірка, в яку буде скопійоване значення 5
+// let b = a;
+// console.log(a); // 5
+// console.log(b); // 5
+
+// // Змінимо значення a
+// a = 10;
+// console.log(a); // 10
+// // Значення b не змінилося, оскільки це окрема копія
+// console.log(b); // 5
+
+// ---------------------------------------складні типи (об'єкти, масиви, функції) присвоюються за посиланням---------------------------------
+
+// const a = ["Mango"];
+// // Оскільки a - це масив, в b записується посилання на вже існуючий
+// // масив в пам'яті. Тепер a і b вказують на той самий масив.
+// const b = a;
+// console.log(a); // ["Mango"]
+// console.log(b); // ["Mango"]
+
+// // Змінимо масив, додавши ще один елемент, використовуючи вказівник з a
+// a.push("Poly");
+// console.log(a); // ["Mango", "Poly"]
+
+// // b також змінилось, тому що b, як і a,
+// // просто містить посилання на те ж саме місце в пам'яті
+// console.log(b); // ["Mango", "Poly"]
+
+// // Результат повторюється
+// b.push("Ajax");
+// console.log(a); // ["Mango", "Poly", "Ajax"]
+// console.log(b); // ["Mango", "Poly", "Ajax"]
+
+// --------------------------------Методи split() і join()-----------------------------
+
+// ----------------------split----------------------------
+// const name = "Mango";
+// console.log(name.split(""));
+
+// const message = "JavaScript - це цікаво";
+// console.log(message.split(" "));
+
+// ------------------------join------------------------------
+// const words = ["JavaScript", "це", "цікаво"];
+// console.log(words.join("")); // "JavaScriptцецікаво"
+// console.log(words.join(" ")); // "JavaScript це цікаво"
+// console.log(words.join("-")); // "JavaScript-це-цікаво"
+
+// ------------------------------------Метод indexOf()------------------------
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.indexOf("Kiwi")); // 2
+// console.log(clients.indexOf("Monkong")); // -1
+
+// ----------------------------------Метод includes()--------------------------
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.includes("Poly")); // true
+// console.log(clients.includes("Monkong")); // falseconst clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.includes("Poly")); // true
+// console.log(clients.includes("Monkong")); // false
+
+// -----------------------------------------Перевірка багатьох умов з includes()-------------------------------------------------
+// const redFruits = ["apple", "strawberry", "cherry", "cranberries"];
+// const fruit = "pineaaple";
+
+// const hasFruit = redFruits.includes(fruit);
+// if (hasFruit) {
+//     console.log(`${fruit} is a red fruit`);
+// }
+// else { console.log(`${fruit} is not red fruit`); }
+
+// --------------------------------------Методи push() і pop()--------------------------------
+
+// --------------------------push() -----------------------
+// const numbers = [];
+// numbers.push(1);
+// console.log(numbers); // [1]
+
+// numbers.push(2);
+// console.log(numbers); // [1, 2]
+
+// numbers.push(3);
+// console.log(numbers); // [1, 2, 3]
+
+// numbers.push(4);
+// console.log(numbers); // [1, 2, 3, 4]
+
+// numbers.push(5);
+// console.log(numbers); // [1, 2, 3, 4, 5]
+
+// --------------------------pop() -----------------------
+// const numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers.pop());
+// console.log(numbers);
+
+// console.log(numbers.pop()); //  4
+// console.log(numbers); // [1, 2, 3]
+
+// console.log(numbers.pop()); //  3
+// console.log(numbers); // [1, 2]
+
+// console.log(numbers.pop()); //  2
+// console.log(numbers); // [1]
+
+// console.log(numbers.pop()); //  1
+// console.log(numbers); // []
+
+// --------------------------------------------Метод slice()--------------------------------------
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.slice(1, 3)); // ["Ajax", "Poly"]
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.slice()); // ["Mango", Ajax", "Poly", "Kiwi"]
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.slice(1)); // ["Ajax", "Poly", "Kiwi"]
+// console.log(clients.slice(2)); // ["Poly", "Kiwi"]
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.slice(-2)); // ["Poly", "Kiwi"]
+
+// -----------------------------------------------Метод splice()---------------------------------------
+
+// Метод splice змінює вихідний масив і повертає масив, що містить видалені елементи.
+// Наприклад, у нас є масив оцінок, який містить п'ять чисел від 1 до 5.
+
+// const scores = [1, 2, 3, 4, 5];
+
+// // Видаляємо три елементи масиву, починаючи з першого елемента (індекс 0)
+// const deletedScores = scores.splice(0, 3);
+
+// // Тепер масив scores містить два елементи
+// console.log(scores); // [4, 5]
+
+// // А масив deletedScores містить три видалені елементи
+// console.log(deletedScores); // [1, 2, 3]
+
+// ------------------------------------------Додавання---------------------------------------------
+// const colors = ["red", "green", "blue"];
+
+// colors.splice(2, 0, "purple");
+// console.log(colors); // ["red", "green", "purple", "blue"]
+
+// ----------------------Заміна-----------------------------
+
+// const languages = ["C", "C++", "Java", "JavaScript"];
+
+// // Заміняємо елемент з індексом 1 на новий
+// languages.splice(1, 1, "Python");
+// console.log(languages); // ["C", "Python", "Java", "JavaScript"]
+
+// // Заміняємо один елемент (з індексом 2) на декілька
+// languages.splice(2, 1, "C#", "Swift", "Go");
+// console.log(languages); // ["C", "Python", "C#", "Swift", "Go", "JavaScript"]
+
+// ------------------------------------------Метод concat()-------------------------------------------
+
+// const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// const newClients = ["Monkong", "Singu"];
+
+// const allClientsWithOldFirst = oldClients.concat(newClients);
+// console.log(allClientsWithOldFirst); // ["Mango", "Ajax", "Poly", "Kiwi", "Monkong", "Singu"]
+
+// const allClientsWithNewFirst = newClients.concat(oldClients);
+// console.log(allClientsWithNewFirst); // ["Monkong", "Singu", "Mango", "Ajax", "Poly", "Kiwi"]
+
+// console.log(oldClients); // ["Mango", "Ajax", "Poly", "Kiwi"]
+// console.log(newClients); // ["Monkong", "Singu"]
